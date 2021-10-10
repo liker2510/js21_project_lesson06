87acbcdb14b2df4d32eed89d20d85e3438573949
Ilker Karakurt <i_love_history96@mail.ru>
2021-10-10 14:34:17 +0500

    Выплнил домашнее задание шестого урока

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-
 'use strict';
-
-let randomNumber = +prompt('Угадай число от 1 до 100');
 
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-const getNumber = function(randomNumberIndex) {
-    if(randomNumberIndex > 25) {
-        alert("Загаданное число меньше");  
-        randomNumber = +prompt('Угадай число от 1 до 100');
-        getNumber(randomNumber);
-    }else if (randomNumberIndex < 25 && randomNumberIndex >= 1) {
-        alert("Загаданное число больше");
-        randomNumber = +prompt('Угадай число от 1 до 100');
-        getNumber(randomNumber);
-    }else if (!isNumber(randomNumberIndex)) {
-        alert("Введи число");
-        randomNumber = +prompt('Угадай число от 1 до 100');
-        getNumber(randomNumber);
-    }else if (randomNumberIndex == "" || randomNumberIndex == null) {
-        alert("Игра окончена");
-    }else if (randomNumberIndex == 25) {
-        alert("Поздравляю, Вы угадали!!");
+const getNumber = function(x) {
+    function getRandomNumber () {
+        let randomNumber = +prompt('Угадай число от 1 до 100');
+        if(randomNumber > x) {
+            alert("Загаданное число меньше");
+            getRandomNumber();
+        }else if (randomNumber < x && randomNumber >= 1) {
+            alert("Загаданное число больше");
+            getRandomNumber();
+        }else if (!isNumber(randomNumber)) {
+            alert("Введи число");
+            getRandomNumber();
+        }else if (randomNumber == "" || randomNumber == null) {
+            alert("Игра окончена");
+        }else if (randomNumber == 25) {
+            alert("Поздравляю, Вы угадали!!");
+        }
     }
+    getRandomNumber();
 }
-getNumber(randomNumber);
+getNumber(25);
